a078d9730d625764c5ae89bd86cf97b41cc45559
muhammedirfan-lap-win <irfannadapuram@gmail.com>
2025-11-20 16:34:35 +0530

    added another tag for analytistic google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YV2VSL5ETE"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-YV2VSL5ETE');
+            `,
+          }}
+        />
         {/* Google Tag Manager  */}
         <script
           dangerouslySetInnerHTML={{
